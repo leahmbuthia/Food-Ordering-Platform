8cfa98964d8c377baaada5e05a2dd386eadba188
Leah Nyambura <leahnyambura682@gmail.com>
2024-07-02 23:43:45 +0300

    React router dom and layout/header setup

---
+++ frontend/src/AppRouter.tsx
@@ -0,0 +1,15 @@
+import {Navigate, Route, Routes}from "react-router-dom";
+import Layout from "./layouts/layout";
+
+
+const AppRoutes =()=>{
+    return(
+        <Routes>
+            <Route path="/" element={<Layout>Home page</Layout>}/>
+            <Route path="/user-profile" element={<span>User profile page</span>}/>
+            <Route path="*" element={<Navigate to="/"/>}/> 
+        </Routes>
+    );
+};
+
+export default AppRoutes;
